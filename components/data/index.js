273781d2864f8745e708bdf03eaba40e62618836
cifernando <fernando.noliveira@mercadolivre.com>
2022-10-20 01:36:37 -0300

    add new description text

--- components/data/index.js
+++ components/data/index.js
@@ -27,8 +27,8 @@ export const about = [
     image: '/fernandoGrandao.jpg',
     title: 'Hoje eu pareço mais velho.',
     title2: 'Eu sou assim.',
-    description: 'Cresci em uma cidade pequena na Bahia chamada Cordeiros.',
+    description: 'Cresci em uma cidade pequena da Bahia chamada Cordeiros.',
     description2: 'Depois de me mudar para Vitória da Conquista e iniciar um curso de Técnico em informática descobri minha paixão por programação em 2013 implementando uma simples página WEB e fazendo meu primeiro "Hello World!" em Java.',
-    description3: 'Hoje eu estudo desenvolvimento WEB na Trybe. No meu tempo livre eu gosto de cuidar das minhas plantas, tirar fotos, tocar violão e cantar.', 
+    description3: 'Trabalhei na mesma escola onde me formei como técnico em informática, e tive a oportunidade de aprender muito sobre como lidar com pessoas e resolver problemas, depois ingressei como desenvolvedor front-end em uma empresa de educação chamada LETRUS que usa de inteligência artificial para ajudar no letramento de crianças e adolescentes, lá aprendi muito sobre como trabalhar em equipe e como tirar o melhor das pessoas de diferentes áreas. Hoje trabalho no mercado libre como desenvolvedor de software.', 
   },
 ];
